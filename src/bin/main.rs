18528edb6ac9893c23e3f469811b60fcfca6f8f3
Riccardo Attilio Galli <riccardo@sideralis.org>
2022-09-07 23:26:11 -0700

    Load images for one file or a directory

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,38 +7,57 @@ fn main() -> Result<()> {
     env_logger::init();
 
     let args: Vec<_> = std::env::args().collect();
-    if args.len() != 3 {
+    if args.len() != 2 {
         return Err(anyhow!("usage: {} IMAGE", args[0]));
     }
 
-    let path = std::path::Path::new(&args[1]);
-    let name = path.file_stem().and_then(|x| x.to_str()).unwrap_or("image");
+    let arg_path = std::path::Path::new(&args[1]);
 
-    let image =
-        image::open(path).map_err(|e| anyhow!("Failed to read image from {:?}: {}", path, e))?;
-    /*
-       let image_rgba8 = image.into_rgba8();
+    let mut paths = Vec::new();
+    if arg_path.is_file() {
+        paths.push(arg_path.canonicalize()?)
+    } else if arg_path.is_dir() {
+        for entry in arg_path
+            .read_dir()?
+            .filter_map(|x| x.ok())
+            .filter(|e| e.path().is_file())
+        {
+            let path = entry.path();
+            if let Ok(_) = image::ImageFormat::from_path(&path) {
+                paths.push(path);
+            }
+        }
+    }
 
-       let img_view = show_image::ImageView::new(
-           show_image::ImageInfo::rgba8(image_rgba8.width(), image_rgba8.height()),
-           image_rgba8.as_bytes(),
-       );
-    */
+    if paths.is_empty() {
+        return Err(anyhow!(
+            "Could not find images in the provided directory: {}",
+            arg_path.display()
+        ));
+    }
 
-    let image_info = show_image::image_info(&image)?;
-    println!("{:#?}", image_info);
+    let mut images = Vec::new();
 
-    let path = std::path::Path::new(&args[2]);
-    let name = path.file_stem().and_then(|x| x.to_str()).unwrap_or("image");
-
-    let image2 =
-        image::open(path).map_err(|e| anyhow!("Failed to read image from {:?}: {}", path, e))?;
+    // load all the images at once! (step 1, make it work...)
+    for path in paths.iter() {
+        let image = image::open(path)
+            .map_err(|e| anyhow!("Failed to read image from {:?}: {}", path, e))?;
+        images.push((path, image));
+    }
 
     let window = generate_window()?;
-    window.set_image(name, image.clone())?;
+
+    // let's start by displaying something
+    window.set_image(
+        &images.first().unwrap().0.to_string_lossy(),
+        images.first().unwrap().1.clone(),
+    )?;
+
+    let mut img_idx = 0;
 
     // Wait for the window to be closed or Escape to be pressed.
     for event in window.event_channel()? {
+        dbg!("have", img_idx);
         if let event::WindowEvent::KeyboardInput(event) = event {
             if !event.is_synthetic
                 && event.input.key_code == Some(event::VirtualKeyCode::Escape)
@@ -50,14 +69,20 @@ fn main() -> Result<()> {
                 && event.input.key_code == Some(event::VirtualKeyCode::Right)
                 && event.input.state.is_pressed()
             {
-                println!("Right pressed!");
-                window.set_image(name, image2.clone())?;
+                img_idx = (img_idx + 1) % images.len();
+                let image = &images[img_idx];
+                window.set_image(&image.0.to_string_lossy(), image.1.clone())?;
             } else if !event.is_synthetic
                 && event.input.key_code == Some(event::VirtualKeyCode::Left)
                 && event.input.state.is_pressed()
             {
-                println!("Right pressed!");
-                window.set_image(name, image.clone())?;
+                img_idx = if img_idx == 0 {
+                    images.len() - 1
+                } else {
+                    (img_idx - 1) % images.len()
+                };
+                let image = &images[img_idx];
+                window.set_image(&image.0.to_string_lossy(), image.1.clone())?;
             }
         }
     }
