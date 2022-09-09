b469004bcf58048aab6235ee50c4de203ccd1f0c
Riccardo Attilio Galli <riccardo@sideralis.org>
2022-09-08 23:34:40 -0700

    Refactor how we load all the images

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,68 @@
 use anyhow::{anyhow, Result};
+use image::DynamicImage;
 use minimg::window::generate_window;
 use show_image::event;
+use std::path::PathBuf;
+
+struct ImagePair(PathBuf, Option<DynamicImage>);
+
+impl ImagePair {
+    fn path_str(&self) -> std::borrow::Cow<str> {
+        self.0.to_string_lossy()
+    }
+
+    fn image_clone(&self) -> Option<DynamicImage> {
+        self.1.clone()
+    }
+}
+
+struct ImagesBag {
+    data: Vec<ImagePair>,
+    current: usize,
+}
+impl ImagesBag {
+    pub fn new(paths: Vec<PathBuf>) -> Result<Self> {
+        if paths.is_empty() {
+            return Err(anyhow!("The list of images to read cannot be empty"));
+        }
+
+        Ok(ImagesBag {
+            data: paths.into_iter().map(|x| ImagePair(x, None)).collect(),
+            current: 0,
+        })
+    }
+
+    pub fn preload(&mut self, n: usize) -> Result<()> {
+        // load all the images at once! (step 1-bis, make it work...)
+        let data: &mut Vec<ImagePair> = self.data.as_mut();
+        for ImagePair(path, wannabe_img) in data.iter_mut().take(n) {
+            let image = image::open(&path)
+                .map_err(|e| anyhow!("Failed to read image from {:?}: {}", path, e))?;
+            *wannabe_img = Some(image);
+        }
+
+        Ok(())
+    }
+
+    pub fn get(&mut self, idx: usize) -> Option<&ImagePair> {
+        if let Some(image_pair) = self.data.get(idx) {
+            self.current = idx;
+            return Some(image_pair);
+        }
+        None
+    }
+
+    pub fn next(&mut self) -> Option<&ImagePair> {
+        self.get(self.current + 1)
+    }
+
+    pub fn prev(&mut self) -> Option<&ImagePair> {
+        if self.current == 0 {
+            return None;
+        }
+        self.get(self.current - 1)
+    }
+}
 
 #[show_image::main]
 fn main() -> Result<()> {
@@ -36,28 +98,20 @@ fn main() -> Result<()> {
         ));
     }
 
-    let mut images = Vec::new();
-
-    // load all the images at once! (step 1, make it work...)
-    for path in paths.iter() {
-        let image = image::open(path)
-            .map_err(|e| anyhow!("Failed to read image from {:?}: {}", path, e))?;
-        images.push((path, image));
-    }
+    let mut images_bag = ImagesBag::new(paths)?;
+    images_bag.preload(10)?;
+    let first_image_pair = images_bag.next().unwrap(); // there is at least one image
 
     let window = generate_window()?;
 
     // let's start by displaying something
     window.set_image(
-        &images.first().unwrap().0.to_string_lossy(),
-        images.first().unwrap().1.clone(),
+        &first_image_pair.path_str(),
+        first_image_pair.image_clone().unwrap(),
     )?;
-
-    let mut img_idx = 0;
 
     // Wait for the window to be closed or Escape to be pressed.
     for event in window.event_channel()? {
-        dbg!("have", img_idx);
         if let event::WindowEvent::KeyboardInput(event) = event {
             if !event.is_synthetic
                 && event.input.key_code == Some(event::VirtualKeyCode::Escape)
@@ -69,20 +123,16 @@ fn main() -> Result<()> {
                 && event.input.key_code == Some(event::VirtualKeyCode::Right)
                 && event.input.state.is_pressed()
             {
-                img_idx = (img_idx + 1) % images.len();
-                let image = &images[img_idx];
-                window.set_image(&image.0.to_string_lossy(), image.1.clone())?;
+                if let Some(image_pair) = images_bag.next() {
+                    window.set_image(&image_pair.path_str(), image_pair.image_clone().unwrap())?;
+                }
             } else if !event.is_synthetic
                 && event.input.key_code == Some(event::VirtualKeyCode::Left)
                 && event.input.state.is_pressed()
             {
-                img_idx = if img_idx == 0 {
-                    images.len() - 1
-                } else {
-                    (img_idx - 1) % images.len()
-                };
-                let image = &images[img_idx];
-                window.set_image(&image.0.to_string_lossy(), image.1.clone())?;
+                if let Some(image_pair) = images_bag.prev() {
+                    window.set_image(&image_pair.path_str(), image_pair.image_clone().unwrap())?;
+                }
             }
         }
     }
